d283abd82eb768088b37fa53f436af687a7ade03
Uhameed78 <hameedulla.umar@gmail.com>
2026-03-16 12:39:40 +0200

    added texts

--- app/src/main/java/com/example/inputoutput/MainActivity.kt
+++ app/src/main/java/com/example/inputoutput/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.inputoutput
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +9,10 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,11 +22,20 @@ class MainActivity : AppCompatActivity() {
         val name = findViewById<EditText>(R.id.name)
         val world = findViewById<TextView>(R.id.world)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        clickBtn?.setOnClickListener {
+            world.text = "Hello,${name.text}!"
+            Toast.makeText(
+                this@MainActivity,
+                "Button clicked", Toast.LENGTH_SHORT
+            ).show()
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+
         }
     }
 }
-//read mm page 40//
+
